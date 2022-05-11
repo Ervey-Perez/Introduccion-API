3b107f4aeb4d12428d53b557674f4d0e1caebc98
Ervey <erveyperez23@gmail.com>
2022-05-11 11:12:12 -0600

    Nucleo del servidor, parte mas importante.

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var express = require('express');
-var app = express();
+// Require packages and set the port
+const express = require('express');
+const port = 3002;
+//para permitir el manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes')
+const app = express();
 
-app.get('/hola', function(req, res){
-    res.send('Hola Mundo!');
-});
+//usar Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
-app.get('/adios', function(req, res){
-    res.send('Adios Mundo!');
-});
+routes(app);
 
-app.listen(3000, function (){
-    console.log('Aplicacion ejemplo, escuchando el puerto 3000');
+//Iniciar el servidor
+const server = app.listen(port, (error)=>{
+    if(error) return console.log(`Error: ${error}`);
+
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
 });
