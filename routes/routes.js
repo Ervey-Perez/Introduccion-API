b70273f310f3d0cab425fdc02b5f1d586e05a17c
Ervey <erveyperez23@gmail.com>
2022-05-21 15:47:57 -0600

    Campos agregados para actualizar usuarios.

--- routes/routes.js
+++ routes/routes.js
@@ -27,7 +27,7 @@ const router = app => {
             if(error) throw error;
 
             response.send(result);
-        });        
+        });
     });
 
     //Agregar un nuevo usuario
@@ -42,15 +42,14 @@ const router = app => {
     //Actualizar un usuario existente
     app.put('/users/:id', (request, response) => {
         const id = request.params.id;
-
-        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) =>{
-            if(error) throw error;
-
+	const {nombre,apellido}=request.body[0];
+        pool.query(`UPDATE users SET nombre = '${nombre}', apellido = '${apellido}' WHERE (id = ${id});`, (error, result) => {
+            if (error) throw error;
             response.send('User updated successfully.');
         });
     });
 
-    //Eliminar un usuario
+//Eliminar un usuario
     app.delete('/users/:id', (request, response) => {
         const id = request.params.id;
 
@@ -61,5 +60,5 @@ const router = app => {
     });
 }
 
-// Exportar el router
+//Exportar el router
 module.exports = router;
